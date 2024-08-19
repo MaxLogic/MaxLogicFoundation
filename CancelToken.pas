6cb27239ccabda656e249b9faaf846fe74fe9c34
Pawel Piotrowski <pawel@maxlogic.eu>
2024-08-19 02:53:03 +0200

    adding cancelToken.pas

---
+++ CancelToken.pas
@@ -0,0 +1,38 @@
+unit CancelToken;
+
+interface
+
+uses
+  system.sysUtils, system.classes;
+
+Type
+  iCancelToken = Interface
+    procedure Cancel;
+    function GetCanceled: Boolean;
+    property Canceled: Boolean read GetCanceled;
+  end;
+
+  TCancelToken = class(TInterfacedObject, iCancelToken)
+  private
+    fCanceled: Boolean;
+    function GetCanceled: Boolean;
+  public
+    procedure Cancel;
+    property Canceled: Boolean read GetCanceled;
+  end;
+
+implementation
+
+{ TCancelToken }
+
+procedure TCancelToken.Cancel;
+begin
+  fCanceled := True;
+end;
+
+function TCancelToken.GetCanceled: Boolean;
+begin
+  Result := fCanceled;
+end;
+
+end.
