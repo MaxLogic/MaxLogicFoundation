4b45078b88d95e0767b38f64b9e38b536cf0d4ce
Pawel Piotrowski <pawel@maxlogic.eu>
2026-03-14 19:47:50 +0100

    madExcept bugreporting per webupload

--- MaxMadExcept.pas
+++ MaxMadExcept.pas
@@ -1,169 +1,221 @@
 unit MaxMadExcept;
 
 interface
+
 uses
-  windows, SysUtils, madExcept, classes;
+  madExcept,
+  System.SysUtils, system.Classes, generics.collections, system.syncObjs;
 
+procedure AdjustMadExcept(const aBugReportDir: String);
+procedure AddFileToMadExcept(const FileName: string);
 
-procedure AdjustMadExcept(const afLogdir : string);
-Procedure AddFileToMadExcept(const FileName : String);
-procedure Translate(mcfg : IMEModuleSettings);
+procedure UpdateMailSubject(const ErrorMessage: string);
+procedure handleException(const exceptIntf: IMEException; var handled: boolean);
+procedure MadExceptOnBeforeSendMail(Action: TExceptAction;
+  const exceptIntf: IMEException;
+  var handled: boolean);
 
 implementation
+
 uses
-  bsUTILS, pawel1, GlobalConst, iniFiles, Forms, dialogs;
+  maxLogic.IOUtils, maxLogic.madExcept, maxAsync,
+  MaxLogic.MadExceptStrings, autoFree, system.ioUtils;
+
+var
+  fBuildInfo: string = '';
+
+resourcestring
+cMStr_MailBody =
+    'Error: %exceptMsg%' + '%lf%' +
+    'Exe: %appname%' + '%lf%' +
+    'Version: %appversion%' + '%lf%' +
+    'ErrorDetails: %errorDetails%';
 
 
-
-procedure Translate(mcfg : IMEModuleSettings);
+procedure MadExceptOnBeforeSendMail(Action: TExceptAction;
+  const exceptIntf: IMEException;
+  var handled: boolean);
+var
+  s: string;
 begin
-//  with mcfg do
-//  begin
+  if Action = eaSendBugReport then
+  begin
+    // In some situations you might want to check whether the current exception is already stored somewhere.
+    // Comparing exceptions is not too easy, though. Especially comparing callstacks is tough.
+    // So madExcept offers you some help by adding a "CallstackCrc" property to the "IMEException" interface. It consists of 3 dwords.
+    // The first dword designates the exception location only.
+    // The second dword is a crc of the callstack of the crashed thread.
+    // The third dword is a crc of the callstack of all delphi threads.
+    // Please note that this information is only available after all the callstacks were fully calculated.
+    // So asking this property before the "epCompleteReport" phase doesn't make too much sense.
+    // property IMEException.CallstackCrc [index: integer] : dword;
 
-//
-//
-//   CUSTOM STRINGS
-//
-//   except box
-//  property IMESettings.TitleBar           : string;  // title bar
-//  property IMESettings.ExceptMsg          : string;  // exception message
-//  property IMESettings.FrozenMsg          : string;  // frozen message
-//  property IMESettings.BitFaultMsg        : string;  // bit fault message
-//
-//   except box - button captions
-//  property IMESettings.SendBtnCaption     : string;
-//  property IMESettings.SaveBtnCaption     : string;
-//  property IMESettings.PrintBtnCaption    : string;
-//  property IMESettings.ShowBtnCaption     : string;
-//  property IMESettings.ContinueBtnCaption : string;
-//  property IMESettings.RestartBtnCaption  : string;
-//  property IMESettings.CloseBtnCaption    : string;
-//
-//   message box - button captions
-//  property IMESettings.OkBtnCaption       : string;
-//  property IMESettings.DetailsBtnCaption  : string;
-//
-//   please wait box - strings
-//  property IMESettings.PleaseWaitTitle    : string;
-//  property IMESettings.PleaseWaitText     : string;
+    s := '[ID:' +
+      IntTohex(exceptIntf.CallstackCrc[0], 1) + '.' +
+      IntTohex(exceptIntf.CallstackCrc[1], 1) + '] ErrorMessage: ' +
+      exceptIntf.ExceptMessage;
 
-
-//property IMESettings.MailSubject        : string;  // mail subject
-//property IMESettings.MailBody           : string;  // mail body
-//property IMESettings.SendBoxTitle       : string;  // send box title
-//property IMESettings.PrepareAttachMsg   : string;  // prepare attachments
-//property IMESettings.MxLookupMsg        : string;  // mx lookup
-//property IMESettings.ConnectMsg         : string;  // server connect
-//property IMESettings.AuthMsg            : string;  // authentication
-//property IMESettings.SendMailMsg        : string;  // send mail
-//property IMESettings.FieldsMsg          : string;  // set http fields
-//property IMESettings.SendAttachMsg      : string;  // sending attachments
-//property IMESettings.SendFinalizeMsg    : string;  // finalization
-//property IMESettings.SendFailureMsg     : string;  // failure message
-
-  
-//  end;
+    UpdateMailSubject(s);
+  end;
 end;
 
+procedure handleException(const exceptIntf: IMEException; var handled: boolean);
+begin
+  UpdateMailSubject(exceptIntf.ExceptMessage);
+end;
 
-procedure AdjustMadExcept(const afLogdir : string);
+procedure AdjustMadExcept(const aBugReportDir: String);
 var
-  l : TStringList;
-  ini : TiniFile;
-  UploadURL : string;
-  mcfg : IMEModuleSettings;
-  Attachments : IMEAttachments ;
-  ZipFileName : string;
+  mcfg: IMEModuleSettings;
+  lSettings: IMESettings;
+  lAttachments: IMEAttachments;
+  LZipFileName: string;
 
-  procedure Add(const LogName : String);
+  procedure Add(const aFileName: string);
   begin
-    Attachments.Add(afLogdir + LogName,
-                              LogName,
-                              ZipFileName,
-                              '');
+    lAttachments.Add(aFileName,
+      extractFileName(aFileName),
+      LZipFileName,
+      '');
   end;
 
+var
+  s: string;
+  lTranslations: TStringList;
 begin
+  // madexcept.RegisterExceptionHandler(handleException, stDontSync, epQuickFiltering);
+  madExcept.RegisterExceptActionHandler(MadExceptOnBeforeSendMail, stDontSync);
 
-  ZipFileName := 'Log.zip';
+  LZipFileName := 'Log.zip';
   mcfg := MESettings;
-  Translate(mcfg);
+  lSettings := MESettings();
 
-  mcfg.BugReportFile := afLogdir + 'bugreport.log';
-  mcfg.BugReportZip := ZipFileName;
-  mcfg.ScreenShotDepth := 4;
+  mcfg.BugReportFile := TPath.Combine(aBugReportDir, 'bugreport.Log');
+  mcfg.BugReportZip := LZipFileName;
+  //mcfg.ScreenShotDepth := 4;
   mcfg.ScreenShotAppOnly := True;
   mcfg.ScreenShotSendAs := 'Screen.jpg';
-  mcfg.ScreenShotZip          := 'screen.zip';
+  mcfg.ScreenShotZip := 'screen.zip';
 
-
-
-
-//   mcfg.CheckFileCrc := True;
-  mcfg.AutoSave           := True;
-  mcfg.AutoClipboard      := pawel1.IsDelphiRunning;
-  mcfg.ShowPleaseWaitBox  := True;
-
-
-
+  // mcfg.Enabled := True;
+  // mcfg.CheckFileCrc := True;
+  mcfg.AutoSave := True;
+  // mcfg.AutoClipboard := pawel1.IsDelphiRunning;
+  mcfg.ShowPleaseWaitBox := True;
 
   // Send mail Options
-  mcfg.AutoSend           := True;
-//  mcfg.AutoSendPrgrBox    := True;
+  fBuildInfo := Trim(ExtractFileName(ParamStr(0)) + ' ' +
+    maxLogic.IOUtils.GetBuildInfo);
+  mcfg.MailSubject := 'Bugreport - ' + fBuildInfo;
+  mcfg.AutoSend := True;
 
-  mcfg.MailAddr := 'info@maxlogic.eu';
-  // mcfg.MailFrom := 'info@maxlogic.eu';
-  mcfg.MailSubject := 'BugReport - '+ExtractFileName(Application.ExeName);
-
-  mcfg.MailAsSmtpClient  := True;
-  mcfg.SmtpServer := 'mail.maxlogic.eu';
-  mcfg.SmtpPort := 26;
-  mcfg.SmtpAccount := 'bugreport@maxlogic.eu';
-  mcfg.SmtpPassword := 'BugReportAbc123';
-
-
-//
-//  mcfg.MailAsSmtpServer   := false;
-//  mcfg.MailViaMailto     := false;
-//  mcfg.MailViaMapi       := false;
-//    mcfg.UploadViaHttp     := false;
+  maxLogic.madExcept.SetUpWebUpload('https://maxlogic.eu/bugreport_mailer/bugreport_mailer.php', 'maxlogic');
 
   mcfg.HideUglyItems := True;
+  lAttachments := mcfg.AdditionalAttachments;
+  // add('somefile');
 
+  gc(lTranslations, TStringList.Create);
+  lTranslations.Values['MailBody']:= cMStr_MailBody ;
+  MaxLogic.MadExceptStrings.Translate(mcfg, lTranslations);
 
-
-  attachments := mcfg.AdditionalAttachments;
-//  add('ORAuto-Trunc-Strings.log');
-
-
-  attachments := NIL;
-  mcfg := NIL;
+  lAttachments := nil;
+  mcfg := nil;
 end;
 
-
-
-
-Procedure AddFileToMadExcept(const FileName : String);
 var
-  mcfg : IMEModuleSettings;
-  Attachments : IMEAttachments ;
-  ZipFileName : string;
+  glSyncMadExceptFileAdder: TFixedCriticalSection = nil;
+  glMadExceptAttachments: TDictionary<string, BYTE> = nil;
+
+procedure AddFileToMadExcept(const FileName: string);
+var
+  mcfg: IMEModuleSettings;
+  Attachments: IMEAttachments;
+  ZipFileName: string;
+  X: integer;
+  found: boolean;
+  LoFn: string;
+  lCS: TFixedCriticalSection;
+  lDic: TDictionary<string, BYTE>;
+begin
+  // locking free create of critical section
+  if glSyncMadExceptFileAdder = nil then
+  begin
+    lCS := TFixedCriticalSection.Create;
+    if TInterlocked.CompareExchange(Pointer(glSyncMadExceptFileAdder), Pointer(lCS), nil) <> nil then
+      lCS.Free;
+  end;
+  // locking free create of dictionary
+  if glMadExceptAttachments = nil then
+  begin
+    lDic := TDictionary<string, BYTE>.Create;
+    if TInterlocked.CompareExchange(Pointer(glMadExceptAttachments), Pointer(lDic), nil) <> nil then
+      lDic.Free;
+  end;
+
+  LoFn := AnsiLowerCase(FileName);
+  found := glMadExceptAttachments.ContainsKey(LoFn);
+  if not found then
+    glSyncMadExceptFileAdder.enter;
+  begin
+    try
+      found := glMadExceptAttachments.ContainsKey(LoFn);
+      if not found then
+      begin
+        glMadExceptAttachments.Add(LoFn, 0);
+
+        mcfg := MESettings;
+
+        // mcfg.Filter1ShowSetting := ssDetailBox;
+        // mcfg.Filter2ShowSetting := ssDetailBox;
+
+        ZipFileName := mcfg.BugReportZip;
+        Attachments := mcfg.AdditionalAttachments;
+
+        Attachments.Add(FileName,
+          ExtractFileName(FileName),
+          ZipFileName,
+          '');
+
+        Attachments := nil;
+        mcfg := nil;
+      end;
+    finally
+      glSyncMadExceptFileAdder.Leave;
+    end;
+  end;
+end;
+
+procedure UpdateMailSubject(const ErrorMessage: string);
+var
+  mcfg: IMEModuleSettings;
 begin
   mcfg := MESettings;
-  ZipFileName := mcfg.BugReportZip;
-  attachments := mcfg.AdditionalAttachments;
-
-  Attachments.Add(FileName,
-        ExtractFileName(FileName),
-        ZipFileName,
-        '');
-
-  attachments := NIL;
-  mcfg := NIL;
-
+  mcfg.MailSubject := 'Bugreport - ' + fBuildInfo + ' - ' + ErrorMessage;
 end;
 
+initialization
 
+  // This function hides all leaks in unit "initialization" sections.
+  // It's ok to use this for EXEs, but not recommended for DLLs.
+  // HideInitializationLeaks;
 
+  // hide some common leaks that we can not change...
+  HideLeak('TJvCustomComboEdit.DefaultImages');
+
+finalization
+
+  if glSyncMadExceptFileAdder <> nil then
+  begin
+    glSyncMadExceptFileAdder.Free;
+    glSyncMadExceptFileAdder := nil;
+  end;
+  if assigned(glMadExceptAttachments) then
+  begin
+    glMadExceptAttachments.Free;
+    glMadExceptAttachments := nil;
+  end;
 
 end.
+
+
